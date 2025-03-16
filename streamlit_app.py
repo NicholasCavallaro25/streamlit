a5e862b04b973e20b377569683381e413c934cbf
NicholasCavallaro25 <ncavalla@uncc.edu>
2025-03-16 19:51:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,15 +43,6 @@ option = st.selectbox(
 
 st.write("### (2) add a multi-select for Sub_Category *in the selected Category (1)* (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)")
 
- sub_categories = df[df['Category'] == category]['Sub_Category'].unique()
-
-    # Multi-select for Sub_Category
-    selected_sub_categories = st.multiselect(
-        "Select Sub-Categories",
-        sub_categories,
-        sub_categories[:2]  # Default selects first two items
-    )
-
 st.write("### (3) show a line chart of sales for the selected items in (2)")
 st.write("### (4) show three metrics (https://docs.streamlit.io/library/api-reference/data/st.metric) for the selected items in (2): total sales, total profit, and overall profit margin (%)")
 st.write("### (5) use the delta option in the overall profit margin metric to show the difference between the overall average profit margin (all products across all categories)")
