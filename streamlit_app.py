17d52514468fa569ca51127221d90d80074f9be0
NicholasCavallaro25 <ncavalla@uncc.edu>
2025-03-16 19:53:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ option = st.selectbox(
 
 st.write("### (2) add a multi-select for Sub_Category *in the selected Category (1)* (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)")
  # Filter Sub_Categories based on selected Category
-    sub_categories = df[df['Category'] == category]['Sub_Category'].unique()
+        sub_categories = df[df['Category'] == category]['Sub_Category'].unique()
 
     # Multi-select for Sub_Category
     selected_sub_categories = st.multiselect(
